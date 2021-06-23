7d4cb4f80f69cc3b721c6a6407062f91a84356e3
daustindev <daustindev@gmail.com>
2021-06-23 17:09:04 -0500

    Added Tests

--- Competition-BE/CBE/CBEBL/CompBL.cs
+++ Competition-BE/CBE/CBEBL/CompBL.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace CBEBL
 {
-    public class CompBL:ICompBL
+    public class  CompBL:ICompBL
     {
         private readonly Repo _repo;
         public CompBL(CBEDbContext context)
